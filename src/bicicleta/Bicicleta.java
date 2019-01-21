284f05b3612c0320e7be3c652db1642b5a3e9adb
alexnro <alexjandron99@gmail.com>
2019-01-21 21:34:04 +0100

    Class Bicicleta done

---
+++ src/bicicleta/Bicicleta.java
@@ -0,0 +1,14 @@
+package bicicleta;
+
+public class Bicicleta {
+
+    private int id = 0;
+
+    public Bicicleta(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
