98d17cf007df54e45614b4b34976aad232a3d5e2
alexnro <alexjandron99@gmail.com>
2019-01-22 03:01:17 +0100

    Added new methods

--- src/estacion/Estacion.java
+++ src/estacion/Estacion.java
@@ -1,6 +1,7 @@
 package estacion;
 
 import bicicleta.Bicicleta;
+import tarjetaUsuario.TarjetaUsuario;
 
 public class Estacion {
 
@@ -9,11 +10,22 @@ public class Estacion {
     private int numeroAnclaje = 0;
     private Bicicleta[] anclajes = null;
 
-    public Estacion(int id, String direccion, int numeroAnclaje, int[] anclajes) {
+    public Estacion(int id, String direccion, int  anclajes) {
         this.id = id;
         this.direccion = direccion;
-        this.numeroAnclaje = numeroAnclaje;
-        this.anclajes = anclajes;
+        this.anclajes = new Bicicleta[anclajes];
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getDireccion() {
+        return this.direccion;
+    }
+
+    public int getNumeroAnclaje() {
+        return this.numeroAnclaje;
     }
 
     public void consultarEstacion() {
@@ -40,13 +52,26 @@ public class Estacion {
 
         for (Bicicleta anclaje: this.anclajes) {
             if (anclaje == null) {
-                this.anclajes = bicicleta[posicion]:
+                this.anclajes = bicicleta[posicion];
                 mostrarAnclaje(bicicleta, numeroAnclaje);
                 break;
             } else {
                 posicion++;
                 numeroAnclaje++;
             }
+        }
+    }
+
+    public void mostrarAnclaje(Bicicleta bicicleta, int numeroAnclaje) {
+        System.out.println("Bicicleta: " + Bicicleta.getId() + " anclada en el anclaje: " + this.anclajes);
+    }
+
+    public Boolean leerTarjetaUsuario (TarjetaUsuario tarjetaUsuario) {
+        return tarjetaUsuario.getActivada();
+    }
+
+    public void retirarBicicleta(TarjetaUsuario tarjetaUsuario) {
+        
         }
     }
 }
